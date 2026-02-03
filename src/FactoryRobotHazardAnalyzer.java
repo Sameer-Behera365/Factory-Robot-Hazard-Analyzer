900f053ced3c87c316a250ffecc4d74c6c096dfb
Sameer-Behera365 <sonubehera2024@gmail.com>
2026-02-03 16:39:52 +0530

    [Sameer] Added: UC6 custom exception handling

--- src/FactoryRobotHazardAnalyzer.java
+++ src/FactoryRobotHazardAnalyzer.java
@@ -1,15 +1,21 @@
 import java.util.Scanner;
+
 public class FactoryRobotHazardAnalyzer {
-    static boolean isValid(int hazard) {
-        return hazard >= 1 && hazard <= 10;
+
+    static void validate(int hazard) throws RobotSafetyException {
+        if (hazard < 1 || hazard > 10) {
+            throw new RobotSafetyException("Hazard level must be between 1 and 10");
+        }
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int hazard = sc.nextInt();
-        if (isValid(hazard)) {
-            System.out.println("Valid hazard level");
-        } else {
-            System.out.println("Invalid hazard level");
+        try {
+            int hazard = sc.nextInt();
+            validate(hazard);
+            System.out.println("Hazard accepted");
+        } catch (RobotSafetyException e) {
+            System.out.println(e.getMessage());
         }
         sc.close();
     }
