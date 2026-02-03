864da434632062226e2c835b2a6c7268ff925488
Sameer-Behera365 <sonubehera2024@gmail.com>
2026-02-03 16:51:03 +0530

    [Sameer] Added: UC8 final OOPS design

--- src/FactoryRobotHazardAnalyzer.java
+++ src/FactoryRobotHazardAnalyzer.java
@@ -1,17 +1,11 @@
 import java.util.Scanner;
 
 public class FactoryRobotHazardAnalyzer {
-
-    static String classifyRisk(int hazard) {
-        if (hazard <= 3) return "LOW";
-        if (hazard <= 7) return "MEDIUM";
-        return "HIGH";
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int hazard = sc.nextInt();
-        System.out.println("Risk Level: " + classifyRisk(hazard));
+        RobotHazardService service = new RobotHazardService();
+        System.out.println("Risk Level: " + service.getRisk(hazard));
         sc.close();
     }
 }
