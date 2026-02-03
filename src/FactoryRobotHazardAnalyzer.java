4c51d6565a23f4699b29dc1c9a97637c26bd2be4
Sameer-Behera365 <sonubehera2024@gmail.com>
2026-02-03 16:11:32 +0530

    [Sameer] Added: UC2 accept robot input

--- src/FactoryRobotHazardAnalyzer.java
+++ src/FactoryRobotHazardAnalyzer.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class FactoryRobotHazardAnalyzer {
     public static void main(String[] args) {
-        System.out.println("Factory Robot Hazard Analyzer");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter hazard level:");
+        int hazard = sc.nextInt();
+        System.out.println("Hazard level entered: " + hazard);
+        sc.close();
     }
 }
