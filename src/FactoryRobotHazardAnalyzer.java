e9a95a636dca9fc55dfe7eec550862581f51e715
Sameer-Behera365 <sonubehera2024@gmail.com>
2026-02-03 16:13:07 +0530

    [Sameer] Added: UC3 calculate hazard risk

--- src/FactoryRobotHazardAnalyzer.java
+++ src/FactoryRobotHazardAnalyzer.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class FactoryRobotHazardAnalyzer {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter hazard level:");
         int hazard = sc.nextInt();
-        System.out.println("Hazard level entered: " + hazard);
+        int risk = hazard * 10;
+        System.out.println("Risk score: " + risk);
         sc.close();
     }
 }
