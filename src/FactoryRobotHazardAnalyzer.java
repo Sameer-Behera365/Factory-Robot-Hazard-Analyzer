86f2c2d8627ac20d0f495376ef0c3e8b18ef5443
Sameer-Behera365 <sonubehera2024@gmail.com>
2026-02-03 16:46:48 +0530

    [Sameer] Added: UC7 risk classification

--- src/FactoryRobotHazardAnalyzer.java
+++ src/FactoryRobotHazardAnalyzer.java
@@ -2,21 +2,16 @@ import java.util.Scanner;
 
 public class FactoryRobotHazardAnalyzer {
 
-    static void validate(int hazard) throws RobotSafetyException {
-        if (hazard < 1 || hazard > 10) {
-            throw new RobotSafetyException("Hazard level must be between 1 and 10");
-        }
+    static String classifyRisk(int hazard) {
+        if (hazard <= 3) return "LOW";
+        if (hazard <= 7) return "MEDIUM";
+        return "HIGH";
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        try {
-            int hazard = sc.nextInt();
-            validate(hazard);
-            System.out.println("Hazard accepted");
-        } catch (RobotSafetyException e) {
-            System.out.println(e.getMessage());
-        }
+        int hazard = sc.nextInt();
+        System.out.println("Risk Level: " + classifyRisk(hazard));
         sc.close();
     }
 }
