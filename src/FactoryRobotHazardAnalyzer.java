7c42d71a73c6a8a8c78b40a204b298ac987c4f50
Sameer-Behera365 <sonubehera2024@gmail.com>
2026-02-03 16:25:11 +0530

    [Sameer] Added: UC5 refactor logic into method

--- src/FactoryRobotHazardAnalyzer.java
+++ src/FactoryRobotHazardAnalyzer.java
@@ -1,14 +1,15 @@
 import java.util.Scanner;
-
 public class FactoryRobotHazardAnalyzer {
+    static boolean isValid(int hazard) {
+        return hazard >= 1 && hazard <= 10;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int hazard = sc.nextInt();
-
-        if (hazard < 1 || hazard > 10) {
-            System.out.println("Invalid hazard level");
-        } else {
+        if (isValid(hazard)) {
             System.out.println("Valid hazard level");
+        } else {
+            System.out.println("Invalid hazard level");
         }
         sc.close();
     }
